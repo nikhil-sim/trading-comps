02536e6982a2321ee50c6e42674dd5e63c3aa581
Nikhil Simhambhatla <nsimhamb@berkeley.edu>
2022-07-04 19:13:58 -0500

    Updated util.py skeleton--added ret_none

--- util.py
+++ util.py
@@ -2,13 +2,14 @@
 Utility functions to help with networked logic. 
 """
 
-def fetch_webpage(url):
+def fetch_webpage(url, ret_none=False):
   """
   Fetches the HTML of the webpage located at the passed in 'url.' Errors and 
   failures are logged. 
    
   args: 
     url (str): The URL of some webpage. 
+    ret_none (bool): Whether or not to return None for testing purposes. 
   
   returns:
     html (str): The HTML of that webpage. None if there are errors or failures. 
@@ -16,7 +17,7 @@ def fetch_webpage(url):
   html = ''
   return html
 
-def fetch_api_data(ticker, data_point):
+def fetch_api_data(ticker, data_point, ret_none=False):
   """
   Fetches the latest measure (on a trailing 12-month basis for financial 
   statement line item data or the last quote for price data) of 'data_point' for 
@@ -25,6 +26,7 @@ def fetch_api_data(ticker, data_point):
   args:
     ticker (str): The ticker of some company. 
     data_point (str): The data point wanted.
+    ret_none (bool): Whether or not to return None for testing purposes. 
   
   returns:
     data (str): The data wanted. Conversion, if needed, is handled by the 
@@ -44,7 +46,7 @@ def db_write(query, parameters=None):
     against.
   """
 
-def db_read(query, parameters=None):
+def db_read(query, parameters=None, ret_none=False):
   """
   Facilitates the execution of 'query' with, if relevant, 'parameters,' 
   resulting in reading records from the database. Errors and failures are 
@@ -54,6 +56,7 @@ def db_read(query, parameters=None):
     query (str): The SQL query facilitating the read. 
     parameters (list): A list of parameters that the query is executed 
     against. 
+    ret_none (bool): Whether or not to return None for testing purposes. 
     
   returns: 
     data (list): A list of tuples where the tuples correspond to the rows 
